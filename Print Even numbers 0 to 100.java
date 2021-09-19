8b937e1179ff660becedcc072991ac82d8ea8b55
Sumegh G. Tembhurne <84231944+Sumegh22@users.noreply.github.com>
2021-09-19 23:55:19 +0530

    Print Even numbers 0 to 100

---
+++ Print Even numbers 0 to 100.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	
+	 System.out.println("This code outputs Even numbers from 0 to 100");
+
+		for(int i=0;i<=100;i=i+2)
+		{
+		    System.out.println("Even num: "+i);
+		    
+		}
+	}
+}
